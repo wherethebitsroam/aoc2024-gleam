6d936591168b3610b8cc064ed852b5b869a4d94f
Jeff Williams <jeff.williams@intelecy.com>
2024-12-07 13:34:30 +0100

    Add day template generator

---
+++ make-day.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+day="$1"
+if [ -z "$day" ]; then
+    day=`date "+%d"`
+    echo "No day given, using today: $day"
+    read -p "Continue [Yn]? " yn
+    if [ "$yn" != "y" -a "$yn" != "" ]; then
+        exit
+    fi
+fi
+
+cat > "src/day$day.gleam" <<__END__
+import gleam/int
+import gleam/io
+import gleam/list
+import gleam/string
+
+pub fn part1(input: String) -> Int {
+  0
+}
+
+pub fn part2(input: String) -> Int {
+  0
+}
+__END__
+
+cat > "test/day${day}_test.gleam" <<__END__
+import day$day
+import gleeunit
+import gleeunit/should
+
+pub fn main() {
+  gleeunit.main()
+}
+
+const example = ""
+
+pub fn part1_test() {
+  day${day}.part1(example)
+  |> should.equal(0)
+}
+
+pub fn part2_test() {
+  day${day}.part2(example)
+  |> should.equal(0)
+}
+__END__
+
+echo "Generated day${day}!"
